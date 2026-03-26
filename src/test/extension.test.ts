210a812ad093d76727498418325503fabf2f634b
Arthurvroum <arthur.mazeau@epitech.eu>
2026-03-26 02:46:47 +0100

    fix: correct test file syntax (\n)

--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -1,1 +1,11 @@
-import * as assert from "assert";\nimport * as vscode from "vscode";\n\nsuite("Extension Test Suite", () => {\n  vscode.window.showInformationMessage("Start all tests.");\n\n  test("Sample test", () => {\n    assert.strictEqual(-1, [1, 2, 3].indexOf(5));\n    assert.strictEqual(-1, [1, 2, 3].indexOf(0));\n  });\n});
+import * as assert from "assert";
+import * as vscode from "vscode";
+
+suite("Extension Test Suite", () => {
+  vscode.window.showInformationMessage("Start all tests.");
+
+  test("Sample test", () => {
+    assert.strictEqual(-1, [1, 2, 3].indexOf(5));
+    assert.strictEqual(-1, [1, 2, 3].indexOf(0));
+  });
+});
